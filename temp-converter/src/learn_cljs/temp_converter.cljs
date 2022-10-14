6b7c2e23fa2965c8209f62143f3e2c0a15d545df
eyewhysee <eyewhysee@gmail.com>
2022-10-14 12:13:10 -0400

    Update temp_converter.cljs (app in 38 lines)

--- temp-converter/src/learn_cljs/temp_converter.cljs
+++ temp-converter/src/learn_cljs/temp_converter.cljs
@@ -1,22 +1,38 @@
-(ns ^:figwheel-hooks learn-cljs.temp-converter
-  (:require
-   [goog.dom :as gdom]))
+(ns learn-cljs.temp-converter
+  (:require [goog.dom :as gdom]                            ;; <1>
+            [goog.events :as gevents]))
 
-(println "This text is printed from src/learn_cljs/temp_converter.cljs. Go ahead and edit it and see reloading in action.")
+(defn f->c [deg-f]                                         ;; <2>
+  (/ (- deg-f 32) 1.8))
 
-(defn multiply [a b] (* a b))
+(defn c->f [deg-c]
+  (+ (* deg-c 1.8) 32))
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(def celsius-radio (gdom/getElement "unit-c"))              ;; <3>
+(def fahrenheit-radio (gdom/getElement "unit-f"))
+(def temp-input (gdom/getElement "temp"))
+(def output-target (gdom/getElement "temp-out"))
+(def output-unit-target (gdom/getElement "unit-out"))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(defn get-input-unit []                                     ;; <4>
+  (if (.-checked celsius-radio)
+    :celsius
+    :fahrenheit))
 
+(defn get-input-temp []
+  (js/parseInt (.-value temp-input)))
 
+(defn set-output-temp [temp]
+  (gdom/setTextContent output-target
+                       (.toFixed temp 2)))
 
-;; specify reload hook with ^:after-load metadata
-(defn ^:after-load on-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn update-output [_]                                    ;; <5>
+  (if (= :celsius (get-input-unit))
+    (do (set-output-temp (c->f (get-input-temp)))
+        (gdom/setTextContent output-unit-target "F"))
+    (do (set-output-temp (f->c (get-input-temp)))
+        (gdom/setTextContent output-unit-target "C"))))
+
+(gevents/listen temp-input "keyup" update-output)          ;; <6>
+(gevents/listen celsius-radio "click" update-output)
+(gevents/listen fahrenheit-radio "click" update-output)
