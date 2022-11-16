f8d02faa44828969b53b60979031662421f877a6
eyewhysee <eyewhysee@gmail.com>
2022-11-16 11:50:05 -0500

    Update exercise_tracker.cljs (copied from author repo)
    https://raw.githubusercontent.com/kendru/learn-cljs/main/code/lesson-28/exercise-tracker/src/learn_cljs/exercise_tracker.cljs

--- exercise-tracker/src/learn_cljs/exercise_tracker.cljs
+++ exercise-tracker/src/learn_cljs/exercise_tracker.cljs
@@ -1,22 +1,101 @@
-(ns ^:figwheel-hooks learn-cljs.exercise-tracker
-  (:require
-   [goog.dom :as gdom]))
+(ns learn-cljs.exercise-tracker
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
+            [reagent.ratom :as ratom]
+            [goog.dom :as gdom]))
 
-(println "This text is printed from src/learn_cljs/exercise_tracker.cljs. Go ahead and edit it and see reloading in action.")
+(defn- date-string [d]
+  (let [pad-zero #(.padStart (.toString %) 2 "0")
+        y (.getFullYear d)
+        m (-> (.getMonth d) inc pad-zero)
+        d (pad-zero (.getDate d))]
+    (str y "-" m "-" d)))
 
-(defn multiply [a b] (* a b))
+(defn initial-inputs []
+  {:date (date-string (js/Date.))
+   :minutes "0"})
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce state
+  (r/atom {:inputs (initial-inputs)
+           :entries {}}))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(def chart-width 400)
+(def chart-height 200)
+(def bar-spacing 2)
 
+(defn get-points [entries]
+  (let [ms-in-day 86400000
+        chart-days 30
+        now (js/Date.now)]
+    (map (fn [i]
+           (let [days-ago (- chart-days (inc i))
+                 date (date-string (js/Date. (- now (* ms-in-day days-ago))))]
+             (get entries date 0)))
+         (range chart-days))))
 
+(defn chart []
+  (let [entries (r/cursor state [:entries])
+        chart-data (ratom/make-reaction
+                    #(let [points (get-points @entries)]
+                       {:points points
+                        :chart-max (reduce max 1 points)}))]
+    (fn []
+      (let [{:keys [points chart-max]} @chart-data
+            bar-width (- (/ chart-width (count points))
+                         bar-spacing)]
+        [:svg.chart {:x 0 :y 0
+                     :width chart-width :height chart-height}
+         (for [[i point] (map-indexed vector points)
+               :let [x (* i (+ bar-width bar-spacing))
+                     pct (- 1 (/ point chart-max))
+                     bar-height (- chart-height (* chart-height pct))
+                     y (- chart-height bar-height)]]
+           [:rect {:key i
+                   :x x :y y
+                   :width bar-width
+                   :height bar-height}])]))))
 
-;; specify reload hook with ^:after-load metadata
-(defn ^:after-load on-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn date-input []
+  (let [val (r/cursor state [:inputs :date])]
+    (fn []
+      [:div.input-wrapper
+       [:label "Day"]
+       [:input {:type "date"
+                :value @val
+                :on-change #(reset! val (.. % -target -value))}]])))
+
+(defn time-input []
+  (let [val (r/cursor state [:inputs :minutes])]
+    (fn []
+      [:div.input-wrapper
+       [:label "Time (minutes)"]
+       [:input {:type "number" :min 0 :step 1
+                :value @val
+                :on-change #(reset! val (.. % -target -value))}]])))
+
+(defn submit-button []
+  [:div.actions
+   [:button {:type "submit"} "Submit"]])
+
+(defn submit-form [state]
+  (let [{:keys [date minutes]} (:inputs state)]
+    (-> state
+        (assoc-in [:entries date] (js/parseInt minutes))
+        (assoc :inputs (initial-inputs)))))
+
+(defn form []
+  [:form.input-form {:on-submit (fn [e]
+                                  (.preventDefault e)
+                                  (swap! state submit-form))}
+   [date-input]
+   [time-input]
+   [submit-button]])
+
+(defn app []
+  [:div.app
+   [chart]
+   [form]])
+
+(rdom/render
+ [app]
+ (gdom/getElement "app"))
